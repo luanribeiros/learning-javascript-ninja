799a7957b79f24d206b6f4609a4b773413e5f26b
Luan Ribeiro <luanribeiros@gmail.com>
2017-07-28 16:52:39 -0300

    ajustes e novas implementações

--- challenge-13/challenge-13.js
+++ challenge-13/challenge-13.js
@@ -1,4 +1,5 @@
 (function(){
+	
 		/*
 	Envolva todo o código desse desafio em uma IIFE.
 	*/
@@ -73,7 +74,7 @@
 	/*
 	Crie um novo array chamado `nordeste`, que tenha os estados do nordeste.
 	*/
-	
+
 	 var nordeste = ['Bahia', 'Sergipe', 'Alagoas', 'Pernambuco', 'Paraíba', 'Rio Grande do Norte', 'Ceará', 'Piauí', 'Maranhão'];
 
 	/*
@@ -95,7 +96,7 @@
 	ficar no mesmo nível que os estados já existentes, não em um array separado.
 	*/
 	
-	Brasil.push(nordeste);
+	Brasil = Brasil.concat(nordeste);
 
 	/*
 	Mostre no console os estados em `newSudeste`.
@@ -118,13 +119,21 @@
 	- `id`: que será o índice do array `brasil`,
 	- `estado`: que será o estado do array `brasil`.
 	*/
-	// ?
+	
+	var newBrasil = [];
+	Brasil.forEach(function(item, index){
+		newBrasil.push({
+			id: index,
+			estado: item
+		});
+	});
 
 	/*
 	Mostre o array `newBrasil` no console
 	*/
 	console.log( '\nnewBrasil:' );
-	// ?
+	
+	console.log(newBrasil);
 
 	/*
 	Percorra o array `brasil` e verifique se os estados tem mais de 7 letras cada,
@@ -134,7 +143,12 @@
 	- "Nem todos os estados tem mais de 7 letras!"
 	*/
 	console.log( '\nTodos os estados de `brasil` tem mais de 7 letras?' );
-	// ?
+	
+	var every = Brasil.every(function(item){
+		return item.length > 7;
+	});
+
+	console.log(every ? 'Sim, todos os estados tem mais de 7 letras!' : 'Nem todos os estados tem mais de 7 letras!');
 
 	/*
 	Percorra o array `brasil` e verifique se o Ceará está incluído, atribuindo o
@@ -145,7 +159,12 @@
 	- "Ceará não foi incluído :("
 	*/
 	console.log( '\nCeará está incluído em `brasil`?' );
-	// ?
+	
+	var some = Brasil.some(function(item){
+		return item === 'Ceará';
+	});
+
+	console.log(some ? 'Ceará está incluído!' : 'Ceará não foi incluído');
 
 	/*
 	Percorra o array `newBrasil` e crie um novo array que some 1 no ID de cada
@@ -153,23 +172,35 @@
 	- "[ESTADO] pertence ao Brasil."
 	Atribua o novo array a uma variável chamada `map`.
 	*/
-	// ?
+	
+	var map = newBrasil.map(function(item, index){
+		return {
+			id: item.id++,
+			estado: item.estado + 'pertence ao Brasil'
+		}
+	});
 
 	/*
 	Mostre no console o array criado acima:
 	*/
 	console.log( '\nnewBrasil agora com mais informações:' );
-	// ?
+	
+	console.log(map);
 
 	/*
 	Filtre o array criado acima, retornando somente os estados que tiverem
 	ID par. Atribua o valor à uma variável chamada `filter`.
 	*/
-	// ?
+	
+	var filter = map.filter(function(item, index){
+		return item.id % 2 === 0;
+	});
 
 	/*
 	Mostre o array filtrado acima no console.
 	*/
 	console.log( '\nEstados com ID par:' );
-	// ?
+	
+	console.log(filter);
+
 })();
