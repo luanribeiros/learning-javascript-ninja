fdbecd05f1b4e57bf18aedea3bc36828e3e98ce6
Luan Ribeiro <luanribeiros@gmail.com>
2017-08-05 02:59:11 -0300

    challenge concluido

--- challenge-18/challenge-18.js
+++ challenge-18/challenge-18.js
@@ -18,7 +18,17 @@
 	- "101.123-131x32"
 	*/
 	console.log( 'Limpando CPFs:' );
-	// ?
+	
+	function cleanCpf( cpf ){
+		return cpf.replace(/\D/g, '');
+	}
+
+	var cpfs = ['049-214 3421-1', '210.458.522-05', '735 500 794 - 22', '101.123-131x32'];
+
+	cpfs.forEach(function( cpf ){
+		console.log( cleanCpf( cpf ));
+	});
+
 
 	/*
 	Usando os CPFs limpos acima, deixe-os com a formatação correta de CPF.
@@ -26,7 +36,10 @@
 	Mostre o resultado no console.
 	*/
 	console.log( '\nFormatando CPFs corretamente:' );
-	// ?
+	
+	cpfs.forEach(function(cpf){
+		console.log( cleanCpf(cpf).replace(/(\d\d\d)(\d\d\d)(\d\d\d)(\d\d)/, '$1.$2.$3-$4') );
+	});
 
 	/*
 	Crie uma expressão regular que faça match com as palavras "junho" ou "julho",
@@ -40,7 +53,10 @@
 	["junho", "julho"]
 	*/
 	console.log( '\nMatch com as palavras "junho" ou "julho" para a frase "Os meses de janeiro, junho e julho começam com a letra j.":' );
-	// ?
+	
+	console.log('Os meses de janeiro, junho e julho começam com a letra j.'.match(
+		/ju[n|l]ho/
+	));
 
 	/*
 	Crie uma expressão regular que faça o match com a abertura de uma tag
@@ -52,7 +68,9 @@
 	["<div>", "<section>", "<blockquote>"]
 	*/
 	console.log( '\nMatch com a abertura de uma tag HTML:' );
-	// ?
+	
+	console.log('<div><section><blockquote>Texto <img /></blockquote></section></div>'
+		.match(/<\w+>/g) );
 
 	/*
 	Crie uma expressão regular que faça o match com uma tag HTML vazia, casando
@@ -64,7 +82,9 @@
 	["<li></li>", "<li></li>", "<span></span>"]
 	*/
 	console.log( '\nMatch com tags HTML vazias (abertura e fechamento da tag):' );
-	// ?
+	
+	console.log('<div><ul><li></li><li></li><li><span></span></li></ul></div>'
+		.match(/<\w+><\/\w+>/g) );
 
 	/*
 	Vamos complicar um pouco agora :D
@@ -89,6 +109,10 @@
 	corretas, para depois aplicar no código ;)
 	*/
 	console.log( '\nFazer replace dos textos das tags:' );
-	// ?
+	
+	console.log('<h1>Título da página</h1><p>Este é um parágrafo</p><footer>Rodapé</footer>'
+		.replace(/ <(\w+)>([^<]+)<\/\w+> /g, 
+			'<$1>O texto dentro da tag "$1" é "$2"</$1>\n' 
+		) );
 
 })();
