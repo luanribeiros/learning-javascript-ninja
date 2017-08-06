ecede299e6739f4310189d324e47c523db4b8c17
Luan Ribeiro <luanribeiros@gmail.com>
2017-08-06 00:21:34 -0300

    concluindo o challenge

--- challenge-19/challenge-19.js
+++ challenge-19/challenge-19.js
@@ -28,7 +28,9 @@
 	no console:
 	*/
 	console.log( 'Regex para números usando o construtor:' );
-	// ?
+
+	var justNumbersRegex = new RegExp('^\\d', 'gm');
+	console.log( justNumbersRegex );
 
 	/*
 	Verifique se a regex acima casa com o texto na variável `text`, mostrando o
@@ -37,7 +39,8 @@
 	*/
 	var text = '10 anos.\n50 discos vendidos.\nE nem 10% dos meus amigos o conhece.';
 	console.log( '\nNúmeros no início da linha do texto:\n' + text, '\n' );
-	// ?
+
+	console.log(text.match( justNumbersRegex ));
 
 	/*
 	- Crie uma regex que case com números no final de uma string. Atribua a
@@ -48,7 +51,9 @@
 	Mostre a regex no console:
 	*/
 	console.log( '\nRegex para números somente no final das linhas:' );
-	// ?
+	
+	var numbersAtTheEnd = / \d+$/gm;
+	console.log(numbersAtTheEnd);
 
 	/*
 	Verifique se a regex acima casa com o texto na variável `otherText`,
@@ -58,7 +63,8 @@
 	*/
 	var otherText = 'Silvio Santos, nome artístico de Senor Abravanel (Rio de Janeiro, 12\n de dezembro de 1930), é um apresentador de televisão e empresário brasileiro.\n Proprietário do Grupo Silvio Santos, que inclui empresas como a Liderança\n Capitalização (administradora da loteria Tele Sena), a Jequiti Cosméticos e o\n Sistema Brasileiro de Televisão (mais conhecido como SBT), Silvio Santos possui\n um patrimônio avaliado em aproximadamente 6\n bilhões de reais.';
 	console.log( '\nNúmeros no final da linha:\n\n', otherText, '\n' );
-	// ?
+	
+	console.log(otherText.match(numbersAtTheEnd));
 
 	/*
 	Vamos criar um método que vai testar se uma classe CSS existe em uma
@@ -85,6 +91,15 @@
 	*/
 	var markup = '<main>\n  <div class="container">\n    <span class="text date"></span>\n    <p class=\'excerpt\'></p>\n  </div>\n</main>';
 	console.log( '\nQuais classes CSS existem na marcação abaixo?\n\n', markup, '\n' );
-	// ?
+	
+	function hasClass(markup, cssClass){
+		var regex = new RegExp( 'class["\'].*' + cssClass + '.*["\']');
+		return regex.test(markup);
+	}
+
+	var classes = [ 'container', 'text', 'date', 'excerpt', 'main'];
+	classes.forEach(function(cssClass){
+		cosole.log( hasClass( markup, cssClass ) + ' para a classe '+ cssClass );
+	});
 
 })();
