91040ecdbc4ec380a54d8ba7b651c1b8ca21552b
Luan Ribeiro <luanribeiros@gmail.com>
2017-09-04 19:35:18 -0300

    mais implementações de modularização no challenge

--- challenge-24/challenge-24.js
+++ challenge-24/challenge-24.js
@@ -12,31 +12,34 @@
 	mesma funcionalidade.
 	*/
 
-	function initialize(){}
-
-	function initEvents(){}
-
 	var $visor = document.querySelector('[data-js="visor"]');
 	var $buttonsNumbers = document.querySelectorAll('[data-js="button-number"]');
 	var $buttonsOperations = document.querySelectorAll('[data-js="button-operation"]');
 	var $buttonCE = document.querySelector('[data-js="button-ce"]');
 	var $buttonEqual = document.querySelector('[data-js="button-equal"]');
 
-	Array.prototype.forEach.call($buttonsNumbers, function(button) {
-		button.addEventListener('click', handleClickNumber, false);
-	});
-	Array.prototype.forEach.call($buttonsOperations, function(button) {
-		button.addEventListener('click', handleClickOperation, false);
-	});
-	$buttonCE.addEventListener('click', handleClickCE, false);
-	$buttonEqual.addEventListener('click', handleClickEqual, false);
+	function initialize(){
+		initEvents();
+	}
+
+	function initEvents(){
+		Array.prototype.forEach.call($buttonsNumbers, function(button) {
+			button.addEventListener('click', handleClickNumber, false);
+		});
+		Array.prototype.forEach.call($buttonsOperations, function(button) {
+			button.addEventListener('click', handleClickOperation, false);
+		});
+		$buttonCE.addEventListener('click', handleClickCE, false);
+		$buttonEqual.addEventListener('click', handleClickEqual, false);
+	}
+
 
 	function handleClickNumber() {
 		$visor.value += this.value;
 	}
 
 	function handleClickOperation() {
-		$visor.value = removeLastItemIfItIsAnOperator($visor.value);
+		$visor.value = removeLastItemIfItIsAnOperator( $visor.value );
 		$visor.value += this.value;
 	}
 
@@ -44,40 +47,46 @@
 		$visor.value = 0;
 	}
 
-	function isLastItemAnOperation(number) {
+	function isLastItemAnOperation( number ) {
 		var operations = ['+', '-', 'x', '÷'];
 		var lastItem = number.split('').pop();
-		return operations.some(function(operator) {
+		return operations.some( function( operator ) {
 			return operator === lastItem;
 		});
 	}
 
-	function removeLastItemIfItIsAnOperator(number) {
-		if(isLastItemAnOperation(number)) {
-			return number.slice(0, -1);
+	function removeLastItemIfItIsAnOperator( string ) {
+		if(isLastItemAnOperation( string )) {
+			return string.slice(0, -1);
 		}
-		return number;
+		return string;
 	}
 
 	function handleClickEqual() {
 		$visor.value = removeLastItemIfItIsAnOperator($visor.value);
 		var allValues = $visor.value.match(/\d+[+x÷-]?/g);
-		$visor.value = allValues.reduce(function(accumulated, actual) {
+		$visor.value = allValues.reduce( calculateAllValues );
+	}
+
+	function calculateAllValues( accumulated, actual ){
 			var firstValue = accumulated.slice(0, -1);
 			var operator = accumulated.split('').pop();
-			var lastValue = removeLastItemIfItIsAnOperator(actual);
-			var lastOperator = isLastItemAnOperation(actual) ? actual.split('').pop() : '';
-			switch(operator) {
-				case '+':
-					return ( Number(firstValue) + Number(lastValue) ) + lastOperator;
-				case '-':
-					return ( Number(firstValue) - Number(lastValue) ) + lastOperator;
-				case 'x':
-					return ( Number(firstValue) * Number(lastValue) ) + lastOperator;
-				case '÷':
-					return ( Number(firstValue) / Number(lastValue) ) + lastOperator;
-			}
-		});
+			var lastValue = removeLastItemIfItIsAnOperator( actual );
+			var lastOperator = isLastItemAnOperation( actual ) ? actual.split('').pop() : '';
+			return doOperation( operator, firstValue, lastValue ) + lastOperator ;
+	}
+
+	function doOperation( operator ){
+		switch( operator ) {
+			case '+':
+				return ( Number( firstValue ) + Number( lastValue ) ) ;
+			case '-':
+				return ( Number( firstValue ) - Number( lastValue ) ) ;
+			case 'x':
+				return ( Number( firstValue ) * Number( lastValue ) ) ;
+			case '÷':
+				return ( Number( firstValue ) / Number( lastValue ) )  ;
+		}
 	}
 
 })(window, document);
