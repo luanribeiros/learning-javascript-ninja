d0d1888bfa629dc1db2c92af671840d7b8e18bbd
Luan Ribeiro <luanribeiros@gmail.com>
2017-09-14 19:58:14 -0300

    Extendendo nossa lib para outros métodos

--- challenge-26/challenge-26.js
+++ challenge-26/challenge-26.js
@@ -23,6 +23,18 @@
 	*/
 	// ?
 
+	function DOM(elements) {
+		this.elements = this.getDOMElements(elements);
+	}
+
+	DOM.prototype.getDOMElements = function getDOMElements(elements) {
+		return document.querySelectorAll(elements);
+	};
+
+	DOM.prototype.on  = function on() {};
+	DOM.prototype.off = function off() {};
+	DOM.prototype.get = function get() {};
+
 	var $a = new DOM('[data-js="link"]');
 	$a.on('click', function(e) {
 		e.preventDefault();
