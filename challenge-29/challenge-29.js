c19acd63b6870b98d1a1205f4fbb367de2e7a3f3
Luan Ribeiro <luanribeiros@gmail.com>
2017-10-22 23:00:53 -0300

    subsyituindo o DOM.prototype pelo this

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -44,61 +44,61 @@
 			return this.element;
 		};
 
-		DOM.prototype.forEach = function forEach() {
+		this.forEach = function forEach() {
 			return  Array.prototype.forEach.apply(this.element, arguments);
 		};
 
-		DOM.prototype.map = function map() {
+		this.map = function map() {
 			return Array.prototype.map.apply(this.element, arguments);
 		};
 
-		DOM.prototype.filter = function filter() {
+		this.filter = function filter() {
 			return Array.prototype.filter.apply(this.element, arguments);
 		};
 
-		DOM.prototype.reduce = function reduce() {
+		this.reduce = function reduce() {
 			return Array.prototype.reduce.apply(this.element, arguments);
 		};
 
-		DOM.prototype.reduceRight = function reduceRight() {
+		this.reduceRight = function reduceRight() {
 			return Array.prototype.reduceRight.apply(this.element, arguments);
 		};
 
-		DOM.prototype.every = function every() {
+		this.every = function every() {
 			return Array.prototype.every.apply(this.element, arguments);
 		};
 
-		DOM.prototype.some = function some() {
+		this.some = function some() {
 			return Array.prototype.some.apply(this.element, arguments);
 		};
 
-		DOM.prototype.isArray = function isArray(param) {
+		this.isArray = function isArray(param) {
 			return Object.prototype.toString.call(param) === '[object Array]';
 		};
 		//Ou instanciando também funciona -> var dom = new DOM(); e usando console.log(dom.isArray([1, 2, 3]));
 		console.log( DOM.prototype.isArray([1, 2, 3]));
 
-		DOM.prototype.isObject = function isObject(param) {
+		this.isObject = function isObject(param) {
 			return Object.prototype.toString.call(param) === '[object Object]';
 		};
 
-		DOM.prototype.isFunction = function isFunction(param) {
+		this.isFunction = function isFunction(param) {
 			return Object.prototype.toString.call(param) === '[object Function]';
 		};
 
-		DOM.prototype.IsNumber = function isNumber(param) {
+		this.IsNumber = function isNumber(param) {
 			return Object.prototype.toString.call(param) === '[object Number]';
 		};
 
-		DOM.prototype.isString = function isString(param) {
+		this.isString = function isString(param) {
 			return Object.prototype.toString.call(param) === '[object String]';
 		};
 
-		DOM.prototype.isBoolean = function isBoolean(param) {
+		this.isBoolean = function isBoolean(param) {
 			return Object.prototype.toString.call(param) === '[object Boolean]';
 		};
 
-		DOM.prototype.isNull = function isNull(param) {
+		this.isNull = function isNull(param) {
 			return Object.prototype.toString.call(param) === '[object Null]'
 			|| Object.prototype.toString.call(param) === '[object Underfined]';
 		};
