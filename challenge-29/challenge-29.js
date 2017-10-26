d4848c71f6f9dc5cca9a58d7b2b2aa957b301c29
Luan Ribeiro <luanribeiros@gmail.com>
2017-10-25 23:49:23 -0300

    criando método isReady, testando se a requisição é válida e fazendo o parse do JSON para objeto javascript, para usarmos os dados

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -37,6 +37,7 @@
   */
 
 	function app() {
+
 		return {
 			init: function init() {
 				console.log('app init');
@@ -46,8 +47,24 @@
 				var ajax = new XMLHttpRequest();
 				ajax.open('GET', '/company.json', true);
 				ajax.send();
+				ajax.addEventListener('onreadystatechange', this.getCompanyInfo, false);
+			},
 
+			getCompanyInfo: function getCompanyInfo() {
+				if(app().isReady.call(this))
+					return;
+
+				var data = JSON.parse(this.responseText);
+				var $companyName = new DOM('[data-js="company-name]"');
+				var $companyPhone = new DOM('[data-js="company-phone]"');
+				$companyName.get()[0].textContent = data.name;
+				$companyPhone.get()[0].textContent = data.phone;
+				},
+
+			isReady: function isReady() {
+				return this.readyState === 4 && this.status === 200;
 			}
+
 		};
 	}
 
