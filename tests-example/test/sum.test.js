5395a163f4305e112381c52fcd36f67ff7b7db3b
Luan Ribeiro <luanribeiros@gmail.com>
2018-01-23 23:27:42 -0200

    criando os primeitos códigos de testes

--- tests-example/test/sum.test.js
+++ tests-example/test/sum.test.js
@@ -1,5 +1,10 @@
 'use strict';
 
+var expect = require('chai').expect;
+var sum = require('../src/sum');
+
 describe('# SUM', function() {
-	it('');
+	it('Should SUM module to be a function', function() {
+
+	});
 });
